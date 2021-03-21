1f7d685181d15f282f9a4b50b1c6228a8ccfd55c
Alexandros Papadakis <a.papadakis@i-docs.com>
2021-03-21 18:17:04 +0200

    Ads dashboard and registry. Fixes traefik

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,11 @@
-export PATH=/usr/sbin/:/usr/bin/:/bin:/usr/libexec/cni/
+#!/bin/sh
 
+export YUM="dnf"
 
 export REPO="docker.io/"
+
+
+$YUM -y install conntrack-tools
 
 
 docker pull ${REPO}rancher/k3s:v1.20.4-k3s1
@@ -28,8 +32,13 @@ docker pull ${REPO}rancher/local-path-provisioner:v0.0.19
 docker pull ${REPO}rancher/metrics-server:v0.3.6
 docker pull ${REPO}rancher/pause:3.1
 
-docker pull ${REPO}kubernetesui/dashboard:v2.0.0
+docker pull ${REPO}kubernetesui/dashboard:v2.2.0
 docker pull ${REPO}kubernetesui/metrics-scraper:v1.0.6
+
+docker pull ${REPO}registry:2.7.1
+
+
+
 
 # quay.io/prometheus/alertmanager tag: v0.21.0
 # jimmidyson/configmap-reload tag: v0.5.0
@@ -37,7 +46,7 @@ docker pull ${REPO}kubernetesui/metrics-scraper:v1.0.6
 # quay.io/prometheus/prometheus tag: v2.24.0
 # prom/pushgateway tag: v1.3.1
 
-docker tag ${REPO}rancher/coredns-coredns:1.8.0 rancher/coredns-coredns:1.8.
+docker tag ${REPO}rancher/coredns-coredns:1.8.0 rancher/coredns-coredns:1.8.0
 docker tag ${REPO}rancher/klipper-helm:v0.4.3 rancher/klipper-helm:v0.4.3
 docker tag ${REPO}rancher/klipper-lb:v0.1.2 rancher/klipper-lb:v0.1.2
 docker tag ${REPO}rancher/library-busybox:1.32.1 rancher/library-busybox:1.32.1
@@ -46,16 +55,32 @@ docker tag ${REPO}rancher/local-path-provisioner:v0.0.19 rancher/local-path-prov
 docker tag ${REPO}rancher/metrics-server:v0.3.6 rancher/metrics-server:v0.3.6
 docker tag ${REPO}rancher/pause:3.1 rancher/pause:3.1
 
-docker tag ${REPO}kubernetesui/dashboard:v2.0.0 kubernetesui/dashboard:v2.0.0
+docker tag ${REPO}kubernetesui/dashboard:v2.2.0 kubernetesui/dashboard:v2.2.0
 docker tag ${REPO}kubernetesui/metrics-scraper:v1.0.6 kubernetesui/metrics-scraper:v1.0.6
 
+docker tag ${REPO}registry:2.7.1 registry:2.7.1
 
-docker save -o k3s-airgap-images-amd64.tar rancher/coredns-coredns:1.8.0 rancher/klipper-helm:v0.4.3 rancher/klipper-lb:v0.1.2 rancher/library-busybox:1.32.1 rancher/library-traefik:1.7.19 rancher/local-path-provisioner:v0.0.19 rancher/metrics-server:v0.3.6 rancher/pause:3.1 kubernetesui/dashboard:v2.0.0 kubernetesui/metrics-scraper:v1.0.6
+
+
+
+docker save -o k3s-airgap-images-amd64.tar rancher/coredns-coredns:1.8.0 rancher/klipper-helm:v0.4.3 rancher/klipper-lb:v0.1.2 rancher/library-busybox:1.32.1 rancher/library-traefik:1.7.19 rancher/local-path-provisioner:v0.0.19 rancher/metrics-server:v0.3.6 rancher/pause:3.1 kubernetesui/dashboard:v2.2.0 kubernetesui/metrics-scraper:v1.0.6 registry:2.7.1
 
 
 mkdir -p /var/lib/rancher/k3s/agent/images/
 cp ./k3s-airgap-images-amd64.tar /var/lib/rancher/k3s/agent/images/
 
+
+mkdir -p /opt/k3s/setup
+
+cp registries.yaml /opt/k3s/setup
+cp namespaces.yml /opt/k3s/setup
+cp security.yml /opt/k3s/setup
+
+yes | cp -f charts/*.yaml /opt/k3s/setup
+yes | cp -f charts/*.tgz /opt/k3s/setup
+
+
+---
 
 mkdir -p /opt/k3s/bin
 cp k3s /opt/k3s/bin/
@@ -67,6 +92,8 @@ cp containerd-shim /opt/k3s/bin/
 cp check-config /opt/k3s/bin/
 
 CDW=`pwd`
+
+export CWD
 
 cd /opt/k3s/bin/
 ln -sf k3s kubectl
@@ -100,10 +127,11 @@ ln -sf cni flannel
 #ln -sf cni iptables
 
 cd $CWD
-#./k3s server --selinux
 
+#--no-deploy
 
 INSTALL_K3S_SKIP_ENABLE=true \
+	INSTALL_K3S_EXEC="--disable=traefik" \
 	INSTALL_K3S_SKIP_START=true \
 	INSTALL_K3S_SKIP_DOWNLOAD=true \
 	INSTALL_K3S_BIN_DIR=/opt/k3s/bin \
@@ -111,4 +139,23 @@ INSTALL_K3S_SKIP_ENABLE=true \
 	INSTALL_K3S_SKIP_SELINUX_RPM=true \
 	/bin/sh ./install.sh 
 
-# /opt/k3s/bin/:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin
+
+echo "PATH=/opt/k3s/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin" >> /etc/systemd/system/k3s.service.env
+
+mkdir -p /etc/rancher/k3s/
+cp /opt/k3s/setup/registries.yaml /etc/rancher/k3s/registries.yaml
+
+systemctl daemon-reload
+systemctl start k3s
+
+echo "Waiting for service to start"
+sleep 30
+
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+
+/opt/k3s/bin/kubectl apply -f /opt/k3s/setup/namespaces.yml
+/opt/k3s/bin/kubectl apply -f /opt/k3s/setup/security.yml
+
+yes | cp -f /opt/k3s/setup/*.tgz /var/lib/rancher/k3s/server/static/charts/
+yes | cp -f /opt/k3s/setup/*.yaml /var/lib/rancher/k3s/server/manifests/
+
